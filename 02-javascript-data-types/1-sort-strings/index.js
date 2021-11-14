34690bc74ca42911db42b8381024ca15683d1e56
Armason <necroorc@gmail.com>
2021-11-14 14:48:16 +0300

    2nd module completed

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const arrClone = [...arr];
+  switch (param) {
+    case 'asc':
+      return arrClone.sort((a, b) =>
+        a.localeCompare(b, ['ru', 'en'], { caseFirst: 'upper' }));
+    case 'desc':
+      return arrClone.sort((a, b) =>
+        b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper' }));
+  }
 }
