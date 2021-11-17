a773dcbc76fad3f2b1d1b7db163933aadc4bc381
Armason <necroorc@gmail.com>
2021-11-17 23:43:25 +0300

    3rd module completed

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function getter(obj) {
+    let result;
+    path
+      .split('.')
+      .forEach(pathChunk => !result ? result = obj[pathChunk] : result = result[pathChunk])
+    return result;
+  }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let resultArr = [];
+  let count;
+  let cachedCodePoint;
+  let letterLimit;
+  function assistant(letter) {
+    if (letter.codePointAt(0) !== cachedCodePoint) {
+      count = 0;
+    }
+    cachedCodePoint = letter.codePointAt(0);
+    letterLimit = cachedCodePoint * size;
+    if (count === letterLimit) {
+      return;
+    }
+    count += cachedCodePoint;
+    resultArr.push(letter);
+  }
+  for (const letter of string) {
+    assistant(letter);
+  }
+return resultArr.join('')
 }
